aa835e4d07c97f1c80f9be446f4b42907ce801ac
F.K.A <soffersfka@gmail.com>
2021-02-11 16:00:42 +0100

    :sparkles: Quick Sort Algorithm completed.

--- src/main/java/sorting/QuickSort.java
+++ src/main/java/sorting/QuickSort.java
@@ -1,4 +1,53 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] test = {5, 3, 4, 2, 1};
+
+        System.out.println(test.length);
+        System.out.println("unsorted: " + Arrays.toString(test));
+
+        // test.length -1, because else the arr is out of bound.
+        QuickSort.quickSort(test, 0, test.length - 1);
+        System.out.println("sorted: " + Arrays.toString(test));
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start >= end) {
+            // it is sorted then.
+            return;
+        }
+
+        int pivotIndex = partition(arr, start, end);
+        int pivotValue;
+
+        // Recursive Divide & Conquer
+        quickSort(arr, start, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, end);
+    }
+
+    public static  int partition(int[] arr, int start, int end) {
+        int pivotIndex = start;
+        int pivotValue = arr[end];
+        for (int i = start; i < end; i++) {
+            if (arr[i] < pivotValue) {
+                swap(arr, i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+        swap(arr, pivotIndex, end);
+        return pivotIndex;
+    }
+
+    public static void swap (int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+//    public static <T> void swap (List<T> l, int i, int j) {
+//        Collections.<T>swap(l, i, j);
+//    }
 }
